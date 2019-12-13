6223e827660022f33aedc1bdf15adf13af2b2b16
Rangarao-thondamalla <t.rangarao007@gmail.com>
2019-12-13 22:28:10 +0530

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,20 @@ import { SignupComponent } from './signup/signup.component';
 import { RouterModule, Routes } from '@angular/router';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports:      [ 
   BrowserModule,
    FormsModule, 
    RouterModule.forRoot([
+     {path:'', component: HomeComponent},
      {path:'login', component : LoginComponent},
      {path:'signup', component: SignupComponent},
      {path:'**' , component : PagenotfoundComponent}
    ])
    ],
-  declarations: [ AppComponent, HelloComponent, HeaderComponent, SignupComponent, PagenotfoundComponent, LoginComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, SignupComponent, PagenotfoundComponent, LoginComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
