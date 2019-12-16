296b222269861a574754fee1feed6294c0895fa6
Rangarao-thondamalla <t.rangarao007@gmail.com>
2019-12-16 22:51:07 +0530

    1 file modified

--- src/app/signup/signup.service.ts
+++ src/app/signup/signup.service.ts
@@ -5,7 +5,7 @@ import { User } from './user';
 @Injectable()
 export class SignupService {
   // url="https://jsonplaceholder.typicode.com/users";
-  url = "https://5df7ba4a4fdcb20014a48cc.mockapi.io/signup";
+  url = "https://5df7ba4a4fdcb20014a483cc.mockapi.io/signup";
   constructor(private http: HttpClient) { }
   enroll(user : User){
     return this.http.post<any>(this.url, user)
