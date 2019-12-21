4a98861ed0aad6ea42e98718ecfe34daff6647c8
Rangarao-thondamalla <t.rangarao007@gmail.com>
2019-12-21 21:20:05 +0530

    2 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,12 +8,20 @@ import { Http } from '@angular/http';
 export class HomeComponent implements OnInit {
 name = "Ranga Rao Thondamalla"
 users : any [];
+url ="https://5df7ba4a4fdcb20014a483cc.mockapi.io/signup";
 constructor(private http : Http) {
-    http.get("https://5df7ba4a4fdcb20014a483cc.mockapi.io/signup")
+   this.http.get(this.url)
     .subscribe(response=> {
       this.users = response.json();
     });
-   }
-  ngOnInit() {
-  }
+}
+deleteUsers(user) {
+      this.http.delete(this.url + '/' + user)
+      .subscribe(response =>{
+        let index = this.users.indexOf(user);
+        this.users.splice(index,1);
+        console.log(user);
+      })
+    };
+  ngOnInit() {}
 }
